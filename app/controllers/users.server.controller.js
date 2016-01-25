c7f44654d0386d82af4670447851170ca987ff15
duncanleung <leungd@gmail.com>
2016-01-25 13:03:46 -0800

    (#2) Set up MongoDB and Server Routes to GET for Single User

--- app/controllers/users.server.controller.js
+++ app/controllers/users.server.controller.js
@@ -25,3 +25,22 @@ exports.list = function(req, res, next) {
     }
   });
 };
+
+exports.read = function(req, res) {
+  res.json(req.user);
+};
+
+exports.userById = function(req, res, next, id) {
+  User.findOne({
+    _id: id
+  },
+  function(err, user) {
+    if(err) {
+      return next(err);
+    }
+    else {
+      req.user = user;
+      next();
+    }
+  });
+};
--- app/routes/users.server.routes.js
+++ app/routes/users.server.routes.js
@@ -3,7 +3,10 @@ var users = require('../../app/controllers/users.server.controller');
 module.exports = function(app) {
   app.route('/users')
     .post(users.create)
-
     .get(users.list);
 
+  app.route('/users/:userId')
+    .get(users.read);
+
+  app.param('userId', users.userById);
 };
