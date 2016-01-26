31cdbc5196680194e4a70de0eda0f041f230216b
duncanleung <leungd@gmail.com>
2016-01-25 16:47:55 -0800

    (#2) Create Signup Page and Route Controllers for Server

--- app/controllers/users.server.controller.js
+++ app/controllers/users.server.controller.js
@@ -1,6 +1,72 @@
 // Handle all requests for user related operations
 
-var User = require('mongoose').model('User');
+var User = require('mongoose').model('User'),
+    passport = require(passport);
+
+var getErrorMessage = function(err) {
+  var message = '';
+  if(err.code) {
+    switch(err.code) {
+      case 11000:
+      case 11001:
+        message = 'Username already exists';
+        break;
+      default:
+        message = 'Something went wrong';
+    }
+  }
+  else {
+    for(var errName in err.errors) {
+      if(err.errors[errName].message) {
+        message = err.errors[errName].message;
+      }
+    }
+  }
+  return message;
+};
+
+exports.renderRegister = function(req, res, next) {
+  if(!req, user) {
+    res.render('register', {
+      title: 'Register Form',
+      messages: req.flash('error')
+    });
+  }
+  else {
+    return res.redirect('/');
+  }
+};
+
+exports.register = function(req, res, next) {
+  if(!req.user) {
+    var user = new User(req.body);
+    var message = null;
+    user.provider = 'local';
+
+    user.save(function(err) {
+      if(err) {
+        var message = getErrorMessage(err);
+        req.flash('error', message);
+        return res.redirect('/register');
+      }
+
+      req.login(user, function(err) {
+        if(err) {
+          return next(err);
+        }
+        return res.redirect('/');
+      });
+    });
+  }
+  else {
+    return res.redirect('/');
+  }
+};
+
+exports.logout = function(req, res) {
+  req.logout();
+  res.redirect('/');
+};
 
 exports.create = function(req, res, next) {
   var user = new User(req.body);
