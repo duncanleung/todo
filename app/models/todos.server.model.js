3b41a86983ac5c8daab51c18b240bf677f53ce30
duncanleung <leungd@gmail.com>
2016-01-28 13:01:46 -0800

    (#4) Set Up Mongoose Todo Schema

---
+++ app/models/todos.server.model.js
@@ -0,0 +1,32 @@
+// Required in /app/config/mongoose.js
+
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+var TodoSchema = new Schema({
+  created: {
+    type: Date,
+    default: Date.now
+  },
+  title: {
+    type: String,
+    default: '',
+    trim: true,
+    required: "Title can't be blank"
+  },
+  comment: {
+    type: String,
+    default: '',
+    trim: true
+  },
+  creator: {
+    type: Schema.ObjectId,
+    ref: 'User'
+  },
+  completed: {
+    type: Boolean,
+    default: false
+  }
+});
+
+mongoose.model('Todo', TodoSchema);
--- config/mongoose.js
+++ config/mongoose.js
@@ -7,5 +7,6 @@ module.exports = function() {
   var db = mongoose.connect(config.db);
 
   require('../app/models/user.server.model');
+  require('../app/models/todos.server.model');
   return db;
 };
