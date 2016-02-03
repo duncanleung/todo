1aef9de6d8ed5f1aaf5dde3f156c913dc266965c
duncanleung <leungd@gmail.com>
2016-02-03 14:40:11 -0800

    (#13) Combine /app/config/passport.js and /app/config/strategies/local.js

--- config/passport.js
+++ config/passport.js
@@ -4,6 +4,8 @@ var User = require('../app/models/user.server.model.js');
 
 module.exports = function(passport) {
 
+  LocalStrategy = require('passport-local').Strategy;
+
   // Sessions Setup - Persistent Login
   // ==============================
   passport.serializeUser(function(user, done) {
@@ -22,5 +24,24 @@ module.exports = function(passport) {
 
   // Login Strategies Setup
   // ==============================
-  require('./strategies/local.js')();
+  passport.use(new LocalStrategy(function(username, password, done) {
+    User.findOne(
+      {username: username},
+      function(err, user) {
+        if (err) {
+          return done(err);
+        }
+        
+        if (!user) {
+          return done(null, false, {message: 'Unknown user'});
+        }
+
+        if (!user.authenticate(password)) {
+          return done(null, false, {message: 'Invalid password'});
+        }
+
+        return done(null, user);
+      }
+    );
+  }));
 };
