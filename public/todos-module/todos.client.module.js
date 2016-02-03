c90a4ca16f04d043caa9cdc791494f1b3a4c76c3
duncanleung <leungd@gmail.com>
2016-02-03 14:08:17 -0800

    (#13) Flatten Angular todo-module Directory

--- public/todos-module/todos.client.module.js
+++ public/todos-module/todos.client.module.js
@@ -1,1 +1,31 @@
-angular.module('todos', []);
+angular.module('todos', [])
+
+.config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+      .when('/todos', {
+        templateUrl: 'todos-module/views/list-todos.client.view.html'
+      })
+      .when('/todos/create', {
+        templateUrl: 'todos-module/views/create-todo.client.view.html'
+      })
+      .when('/todos/:todoId', {
+        templateUrl: 'todos-module/views/view-todo.client.view.html'
+      })
+      .when('/todos/:todoId/edit', {
+        templateUrl: 'todos-module/views/edit-todo.client.view.html'
+      });
+  }
+])
+
+.factory('Todos', ['$resource',
+  function($resource) {
+    return $resource('api/todos/:todoId', {
+      // Set todoId to _id of current data object
+      todoId: '@_id'
+    }, {
+      update: {
+        method: 'PUT'
+      }
+    });
+  }
+]);
