1c16551040211caba6977bfad7ac3d841dcd5b9e
duncanleung <leungd@gmail.com>
2016-02-03 13:22:02 -0800

    (#13) Combine server.js and /config/express.js

--- server.js
+++ server.js
@@ -1,22 +1,67 @@
-// Require NPM Package Dependencies
-var mongoose = require('mongoose');
+// Base Setup
+// ==============================
 
 // Set the server environment
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
+// Require NPM Package Dependencies
+var mongoose = require('mongoose'),
+    express = require('express'),
+    bodyParser = require('body-parser'),
+    passport = require('passport'),
+    flash = require('connect-flash'),
+    session = require('express-session'),
+    morgan = require('morgan');
 
-var config = require('./config/config'),
-    // mongoose = require('./config/mongoose'),
-    express = require('./config/express'),
-    passport = require('./config/passport');
+var config = require('./config/config');
 
-var app = express(),
-    passport = passport();
+var app = express();
 
-// Connect the Mongo Database
+// Passport Config
+require('./config/passport.js')(passport);
+
+
+// Configuration
+// ==============================
+
 mongoose.connect(config.db);
 
+app.use(morgan('dev'));
+  
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(session({
+  saveUninitialized: true,
+  resave: true,
+  secret: 'orangecountcodeschoolallcodeallday'
+}));
+
+app.set('views', './app/views');
+app.set('view engine', 'ejs');
+
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Routes
+app.use(express.static('./public'));
+
+// Server Routes
+require('./app/routes/index.server.routes.js')(app);
+require('./app/routes/users.server.routes.js')(app);
+require('./app/routes/todos.server.routes.js')(app);
+
+// Catchall Route for Angular
+app.use('/*', function(req, res) {
+  res.render('index', {
+    title: 'ToDo App',
+    user: JSON.stringify(req.user)
+  });
+});
+
+// Start the server
+// ==============================
 app.listen(config.port);
 
-module.exports = app;
 console.log(process.env.NODE_ENV + ' server on port', config.port);
