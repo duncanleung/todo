984b32d0a741fb42b91edd1fd86fb93b8719b0e8
duncanleung <leungd@gmail.com>
2016-02-03 12:59:02 -0800

    (#13) Combine server.js and /config/mongoose.js

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
+// Require NPM Package Dependencies
+var mongoose = require('mongoose');
+
+// Set the server environment
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
+
 var config = require('./config/config'),
-    mongoose = require('./config/mongoose'),
+    // mongoose = require('./config/mongoose'),
     express = require('./config/express'),
     passport = require('./config/passport');
 
-var db = mongoose(),
-    app = express(),
+var app = express(),
     passport = passport();
+
+// Connect the Mongo Database
+mongoose.connect(config.db);
 
 app.listen(config.port);
 
