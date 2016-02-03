5752a85036148ce1407ed1223b79258488377074
duncanleung <leungd@gmail.com>
2016-02-02 16:35:39 -0800

    (#13) Combine user.server.routes and user.server.controller

--- app/controllers/users.server.controller.js
+++ app/controllers/users.server.controller.js
@@ -3,28 +3,28 @@
 var User = require('mongoose').model('User'),
   passport = require('passport');
 
-// Return any Mongoose errors
-var getErrorMessage = function(err) {
-  var message = '';
-  if (err.code) {
-    switch (err.code) {
-      case 11000:
-      case 11001:
-        message = 'Username already exists';
-        break;
-      default:
-        message = 'Something went wrong: ' + err;
-    }
-  }
-  else {
-    for (var errName in err.errors) {
-      if (err.errors[errName].message)
-        message = err.errors[errName].message;
-    }
-  }
+// // Return any Mongoose errors
+// var getErrorMessage = function(err) {
+//   var message = '';
+//   if (err.code) {
+//     switch (err.code) {
+//       case 11000:
+//       case 11001:
+//         message = 'Username already exists';
+//         break;
+//       default:
+//         message = 'Something went wrong: ' + err;
+//     }
+//   }
+//   else {
+//     for (var errName in err.errors) {
+//       if (err.errors[errName].message)
+//         message = err.errors[errName].message;
+//     }
+//   }
 
-  return message;
-};
+//   return message;
+// };
 
 // Middleware to run Passport req.isAuthenticated()
 // If user is not logged in, send 401
@@ -37,67 +37,67 @@ exports.requiresLogin = function(req, res, next) {
   next();
 }
 
-// If user is not logged in then show Login
-// Otherwise redirect to '/' (Angular app)
-exports.renderLogin = function(req, res, next) {
-  if (!req.user) {
-    res.render('login', {
-      title: 'ToDo Login',
-      messages: req.flash('error') || req.flash('info')
-    });
-  }
-  else {
-    return res.redirect('/todos');
-  }
-};
+// // If user is not logged in then show Login
+// // Otherwise redirect to '/' (Angular app)
+// exports.renderLogin = function(req, res, next) {
+//   if (!req.user) {
+//     res.render('login', {
+//       title: 'ToDo Login',
+//       messages: req.flash('error') || req.flash('info')
+//     });
+//   }
+//   else {
+//     return res.redirect('/todos');
+//   }
+// };
 
-// If user is not logged in then show Signup
-// Otherwise redirect to '/' (Angular app)
-exports.renderSignup = function(req, res, next) {
-  if (!req.user) {
-    res.render('signup', {
-      title: 'Signup for ToDo',
-      messages: req.flash('error')
-    });
-  }
-  else {
-    return res.redirect('/todos');
-  }
-};
+// // If user is not logged in then show Signup
+// // Otherwise redirect to '/' (Angular app)
+// exports.renderSignup = function(req, res, next) {
+//   if (!req.user) {
+//     res.render('signup', {
+//       title: 'Signup for ToDo',
+//       messages: req.flash('error')
+//     });
+//   }
+//   else {
+//     return res.redirect('/todos');
+//   }
+// };
 
-// Create new user on POST /signup
-exports.signup = function(req, res, next) {
-  if (!req.user) {
-    var user = new User(req.body);
-    var message = null;
-    user.provider = 'local';
-    user.save(function(err) {
-      if (err) {
-        var message = getErrorMessage(err);
-        req.flash('error', message);
-        return res.redirect('/signup');
-      }
+// // Create new user on POST /signup
+// exports.signup = function(req, res, next) {
+//   if (!req.user) {
+//     var user = new User(req.body);
+//     var message = null;
+//     user.provider = 'local';
+//     user.save(function(err) {
+//       if (err) {
+//         var message = getErrorMessage(err);
+//         req.flash('error', message);
+//         return res.redirect('/signup');
+//       }
 
-      // Passport Login
-      req.login(user, function(err) {
-        if (err) {
-          return next(err);
-        }
+//       // Passport Login
+//       req.login(user, function(err) {
+//         if (err) {
+//           return next(err);
+//         }
         
-        return res.redirect('/todos');
-      });
-    });
-  }
-  else {
-    return res.redirect('/todos');
-  }
-};
+//         return res.redirect('/todos');
+//       });
+//     });
+//   }
+//   else {
+//     return res.redirect('/todos');
+//   }
+// };
 
-// Passport req.logout() and send user back to '/'
-exports.logout = function(req, res) {
-  req.logout();
-  res.redirect('/');
-};
+// // Passport req.logout() and send user back to '/'
+// exports.logout = function(req, res) {
+//   req.logout();
+//   res.redirect('/');
+// };
 
 exports.saveOAuthUserProfile = function(req, profile, done) {
   User.findOne({
@@ -134,46 +134,46 @@ exports.saveOAuthUserProfile = function(req, profile, done) {
   );
 };
 
-// Fetch user document and attach to req.user
-// Middleware for app.param
-exports.userByID = function(req, res, next, id) {
-  User.findOne({
-      _id: id
-    },
-    function(err, user) {
-      if (err) {
-        return next(err);
-      }
-      else {
-        req.user = user;
-        next();
-      }
-    }
-  );
-};
+// // Fetch user document and attach to req.user
+// // Middleware for app.param
+// exports.userByID = function(req, res, next, id) {
+//   User.findOne({
+//       _id: id
+//     },
+//     function(err, user) {
+//       if (err) {
+//         return next(err);
+//       }
+//       else {
+//         req.user = user;
+//         next();
+//       }
+//     }
+//   );
+// };
 
-exports.read = function(req, res) {
-  res.json(req.user);
-};
+// exports.read = function(req, res) {
+//   res.json(req.user);
+// };
 
-exports.update = function(req, res, next) {
-  User.findByIdAndUpdate(req.user.id, req.body, function(err, user) {
-    if (err) {
-      return next(err);
-    }
-    else {
-      res.json(user);
-    }
-  });
-};
+// exports.update = function(req, res, next) {
+//   User.findByIdAndUpdate(req.user.id, req.body, function(err, user) {
+//     if (err) {
+//       return next(err);
+//     }
+//     else {
+//       res.json(user);
+//     }
+//   });
+// };
 
-exports.delete = function(req, res, next) {
-  req.user.remove(function(err) {
-    if (err) {
-      return next(err);
-    }
-    else {
-      res.json(req.user);
-    }
-  });
-};
+// exports.delete = function(req, res, next) {
+//   req.user.remove(function(err) {
+//     if (err) {
+//       return next(err);
+//     }
+//     else {
+//       res.json(req.user);
+//     }
+//   });
+// };
--- app/routes/users.server.routes.js
+++ app/routes/users.server.routes.js
@@ -1,31 +1,152 @@
 // Required in /app/config/express.js
 
-var users = require('../../app/controllers/users.server.controller'),
+// var users = require('../../app/controllers/users.server.controller');
+
+var User = require('../../app/models/user.server.model.js'),
   passport = require('passport');
 
-module.exports = function(app) {
-  app.route('/signup')
-    .get(users.renderSignup)
-    .post(users.signup);
+var getErrorMessage = function(err) {
+  var message = '';
+  if(err.code) {
+    switch (err.code) {
+      case 11000:
+      case 11001:
+        message = 'Username already exists';
+        break;
+      default:
+        message = 'Something went wrong: ' + err;
+    }
+  }
+  else {
+    for (var errName in err.errors) {
+      if (err.errors[errName].message)
+        message = err.errors[errName].message;
+    }
+  }
 
+  return message;
+};
+
+
+module.exports = function(app) {
+  // /signup
+  // ==============================
+  app.route('/signup')
+    // If user is not logged in then show Signup
+    .get(function(req, res, next) {
+          if(!req.user) {
+            res.render('signup', {
+              title: 'Signup for ToDo',
+              messages: req.flash('error')
+            });
+          }
+          else {
+            // Otherwise redirect to '/todos' (Angular app)
+            return res.redirect('/todos');
+          }
+    })
+    // Create new user on POST /signup
+    .post(function(req, res, next) {
+          if(!req.user) {
+            var user = new User(req.body);
+            var message = null;
+            user.provider = 'local';
+            user.save(function(err) {
+              if (err) {
+                var message = getErrorMessage(err);
+                req.flash('error', message);
+                return res.redirect('/signup');
+              }
+
+              // Passport Login
+              req.login(user, function(err) {
+                if (err) {
+                  return next(err);
+                }
+                return res.redirect('/todos');
+              });
+            });
+          }
+          else {
+            return res.redirect('/todos');
+          }
+    });
+
+  // /login
+  // ==============================
   app.route('/login')
-    .get(users.renderLogin)
+    // If user is not logged in then show Login
+    .get(function(req, res, next) {
+          if(!req.user) {
+            res.render('login', {
+              title: 'ToDo Login',
+              messages: req.flash('error') || req.flash('info')
+            });
+          }
+          else {
+            // Otherwise redirect to '/todos' (Angular app)
+            return res.redirect('/todos');
+          }
+    })
+    // Check login attempt with Passport
     .post(passport.authenticate('local', {
       successRedirect: '/todos',
       failureRedirect: '/login',
       failureFlash: true
     }));
 
-  app.get('/logout', users.logout);
+  // Passport req.logout() and send user back to '/'
+  app.get('/logout', function(req, res) {
+    req.logout();
+    res.redirect('/');
+  });
 
+  // Single /:userId Routes
   app.route('/users/:userId')
-    .get(users.read)
-    .put(users.update)
-    .delete(users.delete);
+    // Retrieve single user
+    .get(function(req, res) {
+      res.json(req.user);
+    })
+    // Update single user
+    .put(function(req, res, next) {
+      User.findByIdAndUpdate(req.user.id, req.body, function(err, user) {
+        if (err) {
+          return next(err);
+        }
+        else {
+          res.json(user);
+        }
+      });
+    })
+    // Delete single user
+    .delete(function(req, res, next) {
+      req.user.remove(function(err) {
+        if (err) {
+          return next(err);
+        }
+        else {
+          res.json(req.user);
+        }
+      });
+    });
 
   // .param middleware. Handle all routes with param :userId
   // Query database and return req.user if user w' Id exists
-  app.param('userId', users.userByID);
+  app.param('userId', function(req, res, next, id) {
+    User.findOne({
+        _id: id
+      },
+      function(err, user) {
+        if (err) {
+          return next(err);
+        }
+        else {
+          req.user = user;
+          next();
+        }
+      }
+    );
+  });
 
   app.get('/oauth/facebook', passport.authenticate('facebook', {
     failureRedirect: '/login',
