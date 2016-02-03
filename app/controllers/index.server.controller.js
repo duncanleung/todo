83bc16501de08ab5c9dc41bd9e8c93b1473f23fe
duncanleung <leungd@gmail.com>
2016-02-02 16:15:11 -0800

    (#13) Combine index.server.routes and index.server.controller

--- app/controllers/index.server.controller.js
+++
@@ -1,8 +0,0 @@
-// Required in /app/routes/index.server.routes.js
-
-exports.render = function(req, res) {
-    res.render('index', {
-      title: 'ToDo App',
-      user: JSON.stringify(req.user)
-    });
-};
--- app/routes/index.server.routes.js
+++ app/routes/index.server.routes.js
@@ -1,6 +1,12 @@
 // Required in /app/config/express.js
 
 module.exports = function(app) {
-  var index = require('../controllers/index.server.controller');
-  app.get('/', index.render);
+  app.get('/', function(req, res) {
+    res.render('index',
+      {
+        title: 'ToDo App',
+        user: JSON.stringify(req.user)
+      }
+    );
+  });
 };
